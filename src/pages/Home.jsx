3ac8aab98dfd38802622424dce051ba792f0de22
Simone-Domenici <domenici.simone00@gmail.com>
2024-12-10 16:26:05 +0100

    FIX featured posts in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,23 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 function Home() {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:3000/posts`)
+      .then(res => setPosts(res.data.slice(0, 3)))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <main>
       <section className="featured">
         <h2>Featured Posts</h2>
         <div className="post-grid">
-          {/* {featuredPosts.map((post) => (
-            <PostCard key={post.id} post={post} />
-          ))} */}
+          {posts.map((post) => (
+              <PostCard key={post.id} post={post} />
+            ))}
         </div>
         <div className="view-all">
           <Link to="/posts">View All Posts</Link>
